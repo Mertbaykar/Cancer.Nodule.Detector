287b02857a01077e823a0d497e05964b5f9baf98
Mertbaykar <mertbaykar@hotmail.com>
2023-01-19 00:03:37 +0300

    Add project files.

---
+++ ImageProcessor.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using ImageProcessing;
+using System.Drawing;
+
+string path = "D:\\Cancer.Nodule.Detector\\Cancer.Nodule.Detector\\ImageProcessor.Test\\Images\\example.png";
+string filepath2Save = "D:\\Cancer.Nodule.Detector\\Cancer.Nodule.Detector\\ImageProcessor.Test\\ProcessedImages\\example4.png";
+
+try
+{
+    Bitmap image = (Bitmap)Image.FromFile(path);
+    ImageProcessor.ApplyFilter2Image(ref image);
+    image.Save(filepath2Save, System.Drawing.Imaging.ImageFormat.Png);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+
